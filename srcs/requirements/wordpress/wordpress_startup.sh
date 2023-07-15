f7ed4a98130184cc5af1c0295cf0dc28b2fb241a
Stephanie Lakner <stephanie.lakner@rub.de>
2023-07-15 20:12:10 +0200

    Progress

--- srcs/requirements/wordpress/wordpress_startup.sh
+++ srcs/requirements/wordpress/wordpress_startup.sh
@@ -9,9 +9,9 @@ chown -R www-data:www-data /wordpress
 if ! sudo -u www-data wp core is-installed --path=/wordpress; then
 	echo "---Downloading wordpress---"
 	sudo -u www-data wp core download --path=/wordpress
-	sudo -u www-data wp config create --dbhost=mariadb --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --allow-root --force #--skip-check
+	sudo -u www-data wp config create --dbhost=mariadb --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --allow-root --force
 	echo "---Installing wordpress---"
-	sudo -u www-data wp core install --url==slakner.42.fr --title=slakner --admin_user=root --admin_password=root --admin_email=admin@slakner.42.fr
+	sudo -u www-data wp core install --url==${WP_URL} --title=slakner --admin_user=root --admin_password=root --admin_email=admin@slakner.42.fr
 else
 	echo "---Wordpress already installed, skipping installation---"
 fi
@@ -19,4 +19,5 @@ fi
 
 #php wp --info
 #tail -f
-/usr/sbin/php-fpm8.2 -F
+#exec strace -e trace=read,write /usr/sbin/php-fpm8.2 -F --allow-to-run-as-root
+exec /usr/sbin/php-fpm8.2 -F --allow-to-run-as-root
